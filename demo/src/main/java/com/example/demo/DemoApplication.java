d1202bdb8bae37f0324f064324f0f7407b140ce9
phillus33 <philipp.roth33@gmail.com>
2022-07-04 20:32:43 +0200

    added student controller + service

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -11,25 +11,12 @@ import java.time.Month;
 import java.util.*;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(
-				new Student(
-						1L,
-						"Mariam",
-						"mariam.jamal@gmail.com",
-						LocalDate.of(2000, 1, 5),
-						21
 
-				)
-		);
-	}
 
 }
